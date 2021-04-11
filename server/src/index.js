6bc10890bc57c9c7674288c61e34625bfb7b40f4
Jared <jaredkohou@gmail.com>
2021-04-11 18:17:12 -0500

    index

--- server/src/index.js
+++ server/src/index.js
@@ -2,3 +2,8 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
+
+
+// init middlewares
+app.use(express.json({limit: '5mb'})) //no huge files to server
+app.use(cors())//communicate with server
