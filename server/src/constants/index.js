768ab69d763991ca96e81db583f1d6138adda3df
Jared <jaredkohou@gmail.com>
2021-04-12 06:52:34 -0500

    data

--- server/src/constants/index.js
+++ server/src/constants/index.js
@@ -1,2 +1,13 @@
 const {config} = require('dotenv')
 config()
+
+
+
+// export all the variables I get from dot env
+module.exports = {
+    MONGO_URL: process.env.MONGO_URL,
+    PORT: process.env.PORT,
+    SERVER_URL: process.env.SERVER_URL,
+    CLIENT_URL: process.env.CLIENT_URL,
+    SECRET: process.env.SECRET
+}
